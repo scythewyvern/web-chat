bcb1cacbacf1a79d02c9c8447625ec7da34193b8
VoskanianGor <vosckaniangor@gmail.com>
2023-01-24 11:28:50 +0300

    Replace old structure to new using Vite

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   root: true,
   env: {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
+    "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: "latest",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
@@ -23,9 +25,6 @@ module.exports = {
         "**/__tests__/*.{j,t}s?(x)",
         "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
-      env: {
-        jest: true,
-      },
     },
   ],
 };
---
+++ src/components/MainButton.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+interface MainButton {
+  onClick: () => void;
+}
+
+defineProps<MainButton>();
+</script>
+
+<template>
+  <button class="button" @click="onClick">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-block: 15px;
+  background: #757de8;
+  border-radius: 8px;
+  font-size: 16px;
+  line-height: 22px;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.005em;
+  min-width: 340px;
+  color: #ffffff;
+  border: none;
+  outline: none;
+}
+</style>
